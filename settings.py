5bc993ff087cc752c5e405072036f1865a885d2e
Al <Sasha252VNS@gmail.com>
2021-05-24 00:09:34 +0300

    Initial Commit

--- settings.py
+++ settings.py
@@ -1,11 +1,11 @@
 N_GPUS = 2
-PATH_TO_PRETRAIN_MODEL = "./weights/long_train/4"
-PATH_TO_EXPERIMENT = "./self_critic/test_exp"
+PATH_TO_PRETRAIN_MODEL = "../gpt2_summ_rl/output/long_train/4"
+PATH_TO_EXPERIMENT = "./experiments/test_exp"
 N_EPOCHS = 2
 BATCH_SIZE = 4
 ACCUMULATION_STEPS = 2
-DATA_FOLDER = "./CNN-DM/gpt2_1024_data"
-PATH_TO_IDS_FILE = "./CNN-DM/ids.json"
+DATA_FOLDER = "../gpt2_summ_rl/CNN-DM/gpt2_1024_data"
+PATH_TO_IDS_FILE = "../gpt2_summ_rl/CNN-DM/ids.json"
 MAX_GEN_LEN = 100
 STEPS_BTW_VALIDATIONS = 500
 LR = 7.07e-6
