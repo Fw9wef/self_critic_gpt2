6943a12ac89be7cf784d860edb22a8c2306b9646
Al <Sasha252VNS@gmail.com>
2021-05-24 00:21:07 +0300

    Initial Commit

--- utils.py
+++ utils.py
@@ -62,7 +62,8 @@ def generate_abstract(model, batch, max_gen_len=MAX_GEN_LEN, greedy=False,
     batch_size = input_seq.shape[0]
 
     gen_logits = list()
-    generation_finished = torch.zeros((batch_size, 1))
+    generation_finished = torch.zeros((batch_size, 1)).cuda()
+    ones = torch.ones_like(generation_finished).cuda()
     for i in range(max_gen_len):
         outputs = model(input_ids=input_seq, attention_mask=mask, position_ids=seq_inds)
         next_token_logits = outputs[0][:, -1, :]
@@ -71,10 +72,10 @@ def generate_abstract(model, batch, max_gen_len=MAX_GEN_LEN, greedy=False,
         probs = torch.softmax(next_token_logits, dim=-1)
         next_token = torch.multinomial(probs, num_samples=1)
 
-        generation_finished = torch.where(next_token == eos_token, torch.ones_like(generation_finished), generation_finished)
+        generation_finished = torch.where(next_token == eos_token, ones, generation_finished)
         input_seq = torch.cat([input_seq, next_token], dim=-1)
         mask = torch.cat([mask, 1-generation_finished], dim=-1)
-        new_inds = seq_inds[:,-1:] + (1 - generation_finished)
+        new_inds = seq_inds[:, -1:] + (1 - generation_finished)
         seq_inds = torch.cat([mask, new_inds], dim=-1)
 
         if torch.all(generation_finished == 1):
