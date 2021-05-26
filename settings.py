db4c74a89bc4ea89f4dfc27a99e1678b50ddb61e
Al <Sasha252VNS@gmail.com>
2021-05-26 22:20:19 +0300

    Initial Commit

--- settings.py
+++ settings.py
@@ -1,11 +1,11 @@
 N_GPUS = 2
 PATH_TO_PRETRAIN_MODEL = "../gpt2_summ_rl/output/long_train/4"
-PATH_TO_EXPERIMENT = "./experiments/sample_exp_v2"
+PATH_TO_EXPERIMENT = "./experiments/big_sample_exp_v2"
 N_EPOCHS = 2
 BATCH_SIZE = 4
-ACCUMULATION_STEPS = 2
+ACCUMULATION_STEPS = 16
 DATA_FOLDER = "../gpt2_summ_rl/CNN-DM/gpt2_1024_data"
 PATH_TO_IDS_FILE = "../gpt2_summ_rl/CNN-DM/ids.json"
 MAX_GEN_LEN = 100
 STEPS_BTW_VALIDATIONS = 500
-LR = 7.07e-6
+LR = 1e-6
