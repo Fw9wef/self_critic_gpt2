e15f3150176a50db1f3dc670c8eae854f739c939
Fw9wef <56963135+Fw9wef@users.noreply.github.com>
2021-06-27 13:22:12 +0300

    Delete model.py

--- model.py
+++
@@ -1,9 +0,0 @@
-from torch import nn
-
-
-class DataParallel(nn.DataParallel):
-    def __getattr__(self, item):
-        if item == 'save_pretrained':
-            return getattr(self.module, item)
-        else:
-            return super().__getattr__(item)
